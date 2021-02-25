e4612485b26b958b7b1df9ac67b389e32c05b736
Jeff McCoy <jeffrey.mccoy.10@us.af.mil>
2021-02-25 03:56:29 -0600

    work-around for imagePullSecrets for now

--- init.sh
+++ init.sh
@@ -3,33 +3,19 @@ set -e
 
 APPLICATION=hello-world-bb-universe
 IMAGE_CACHE=${HOME}/.dod-platform-one-big-bang-cache
-REGISTRY_CREDS=$(pwd)/bigbang/.iron-bank-creds
+REGISTRY_CREDS=$(pwd)/.iron-bank-creds
 
 function setup_creds() {
   if [ ! -f ${REGISTRY_CREDS}.env ]; then
     cat <<EOF > $REGISTRY_CREDS.env
 IB_USERNAME=REPLACE_ME
 IB_PASSWORD=REPLACE_ME
-
 EOF
     echo "Need to add Iron Brank pull creds.  Edit the file ${REGISTRY_CREDS}.env and then press [spacebar] to continue."
     read -s -d ' '
   fi
 
   source ${REGISTRY_CREDS}.env
-
-  cat <<EOF > $REGISTRY_CREDS-bb.yaml
-apiVersion: v1
-kind: Secret
-metadata:
-    name: common-bb
-stringData:
-    values.yaml: |-
-        registryCredentials:
-          username: $IB_USERNAME
-          password: $IB_PASSWORD
-EOF
-
 }
 
 function launch_k3d() {
@@ -48,14 +34,17 @@ function launch_k3d() {
     ${APPLICATION}
 }
 
-function install_flux() {
-  kubectl create ns flux-system
-
-  # Create the IB pull secret for flux
-  kubectl create secret docker-registry private-registry -n flux-system \
+function hack_secret() {
+  kubectl create secret docker-registry private-registry -n $1 \
+    --dry-run=client \
     --docker-server=registry1.dso.mil \
     --docker-username="${IB_USERNAME}" \
-    --docker-password="${IB_PASSWORD}"
+    --docker-password="${IB_PASSWORD}" -o yaml | kubectl apply -f -
+}
+
+function install_flux() {
+  kubectl create ns flux-system
+  hack_secret "flux-system"
 
   # Install flux in the cluster
   kubectl apply -f https://repo1.dso.mil/platform-one/big-bang/umbrella/-/raw/master/scripts/deploy/flux.yaml
@@ -68,6 +57,14 @@ function install_flux() {
 function deploy_bb() {
   # Launch big bang 
   kubectl apply -f start.yaml
+
+  # Lazy temp workaround 
+  echo "Wait to overwrite pull secrets"
+  sleep 20
+
+  for ns in $(kubectl get ns -o jsonpath="{.items[*].metadata.name}"); do
+    hack_secret $ns
+  done
 
   watch kubectl get kustomizations,hr,po -A
 }
