e2766a4385cb4417e3f3b177878848520baa853f
Jeff McCoy <jeffrey.mccoy.10@us.af.mil>
2021-03-31 12:14:23 -0500

    updates for terraform test

--- init.sh
+++ init.sh
@@ -3,25 +3,6 @@ set -e
 
 APPLICATION=big-bang-quick-start
 IMAGE_CACHE=${HOME}/.dod-platform-one-big-bang-cache
-REGISTRY_CREDS=$(pwd)/.iron-bank-creds.yaml
-
-function setup_creds() {
-  if [ ! -f ${REGISTRY_CREDS} ]; then
-    cat <<EOF > $REGISTRY_CREDS
-configs:
-  registry1.dsop.io:
-    auth:
-      username: REPLACE_ME
-      password: REPLACE_ME  
-  registry1.dso.mil:
-    auth:
-      username: REPLACE_ME
-      password: REPLACE_ME
-EOF
-    echo "Need to add Iron Brank pull creds from https://registry1.dso.mil/.  Edit the file ${REGISTRY_CREDS} and then press [spacebar] to continue."
-    read -s -d ' '
-  fi
-}
 
 function launch_k3d() {
   mkdir -p ${IMAGE_CACHE}
@@ -32,7 +13,6 @@ function launch_k3d() {
   # Create the cluster
   k3d cluster create \
     --volume ${IMAGE_CACHE}:/var/lib/rancher/k3s/agent/containerd/io.containerd.content.v1.content \
-    --volume ${REGISTRY_CREDS}:/etc/rancher/k3s/registries.yaml \
     --volume /etc/machine-id:/etc/machine-id \
     --k3s-server-arg "--disable=metrics-server" \
     --k3s-server-arg "--disable=traefik" \
@@ -41,26 +21,11 @@ function launch_k3d() {
     ${APPLICATION}
 }
 
-function install_flux() {
-  kubectl create ns flux-system
-
-  # Install flux in the cluster
-  kubectl apply -f https://repo1.dso.mil/platform-one/big-bang/umbrella/-/raw/master/scripts/deploy/flux.yaml
-
-  # Wait for flux
-  kubectl wait --for=condition=available --timeout 300s -n "flux-system" "deployment/helm-controller"
-  kubectl wait --for=condition=available --timeout 300s -n "flux-system" "deployment/source-controller"
-}
 
 function deploy_bb() {
-  # Launch big bang 
-  kubectl apply -f start.yaml
-
   # Watch the deployments
   watch kubectl get kustomizations,hr,po -A
 }
 
-setup_creds
 launch_k3d
-install_flux
 deploy_bb
