75c04b99fcc263a943b7fe8f95f861846b42e20a
Rob Kernick <rjkernick@gmail.com>
2021-05-06 09:42:52 -0400

    remove argument that wasn't needed

--- init-k3d.sh
+++ init-k3d.sh
@@ -9,15 +9,12 @@ k3d cluster delete ${APPLICATION} || echo "Cluster already deleted"
 
 mkdir -p ${IMAGE_CACHE}
 
-YOURPUBLICEC2IP=$( curl https://ipinfo.io/ip )
-
 # Create the cluster
 k3d cluster create \
   --volume ${IMAGE_CACHE}:/var/lib/rancher/k3s/agent/containerd/io.containerd.content.v1.content \
   --volume /etc/machine-id:/etc/machine-id \
   --k3s-server-arg "--disable=metrics-server" \
   --k3s-server-arg "--disable=traefik" \
-  --k3s-server-arg "--tls-san=${YOURPUBLICEC2IP}" \
   -p 80:80@loadbalancer \
   -p 443:443@loadbalancer \
   ${APPLICATION}
