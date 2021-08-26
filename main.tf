7f9c844e72b95b21e05e77d479066625016a1388
Andrew Roth <andrew.roth@leapfrog.ai>
2021-08-25 18:36:02 -0700

    add values from BB k3d tests folder

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "big_bang" {
-  source = "git::https://repo1.dso.mil/platform-one/big-bang/terraform-modules/big-bang-terraform-launcher.git?ref=4d629bcc"
+  source = "git::https://repo1.dso.mil/platform-one/big-bang/terraform-modules/big-bang-terraform-launcher.git?ref=7691382f"
 
   big_bang_manifest_file = "bigbang/start.yaml"
   registry_credentials = [{
