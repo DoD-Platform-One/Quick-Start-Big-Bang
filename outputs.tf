14485b08e5e92644b4ceaa63009b006046d3dce4
Andrew Roth <andrew.roth@leapfrog.ai>
2021-08-31 20:18:25 +0000

    Upgrade to Big Bang v1.14.1

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,21 @@
 output "istio_gw_ip" {
-  description = "The IP address of the Istio IngressGateway"
-  //noinspection HILUnresolvedReference - `module.big_bang.external_load_balancer` is a json object and `ip` is one of its keys
-  value = module.big_bang.external_load_balancer.ip
+  description = "DEPRECATED - Kept for backwards compatibility reasons, will be removed later. Returns the IP of the first LoadBalancer found in the istio-system namespace"
+  value = jsondecode(module.big_bang.external_load_balancer)[0].ip
+}
+
+output "external_load_balancer" {
+  description = <<EOF
+JSON array with information on all LoadBalancer services in the istio-system namespace. Example output:
+```
+[
+  {
+    "name": "public-ingressgateway",
+    "ip": "192.0.2.0",
+    "hostname": "null"
+  },
+  {...}
+]
+```
+EOF
+  value = module.big_bang.external_load_balancer
 }
